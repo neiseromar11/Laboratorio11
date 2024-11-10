fc978dd5c9c41bb11dfc3791ea7412feeddeadd1
Neiser omar <49031435+neiseromar11@users.noreply.github.com>
2024-11-09 21:09:09 -0500

    Esto era:(

---
+++ src/sem_11/Sem_11.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sem_11;
+
+import Pantallas.Calculadora;
+
+/**
+ *
+ * @author NEISER
+ */
+public class Sem_11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculadora ventana= new Calculadora();
+         ventana.setLocationRelativeTo(null);
+         ventana.setVisible(true);
+    }
+    
+}
